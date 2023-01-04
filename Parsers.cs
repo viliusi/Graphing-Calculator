cba9f19a27312109ada1f4cf09d489c424a9824b
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-01-04 20:37:28 +0100

    Need to find out how to find index and save that index for a char array

--- Parsers.cs
+++ Parsers.cs
@@ -6,10 +6,10 @@ public class Parsers
     }
     public static void inputHandler(string formula)
     {
-        formula = "f(x) = 2 * ( -0.4 - 3 ) ^ 2 + sin(x + 3)";
+        formula = "f(x) = 2.4 + ( -0.4 - 3 ) ^ 2 + sin( x + 3 )";
 
         formula = formula.Replace(" ", "");
-        
+
         string[] nameSplit = formula.Split('=');
 
         Program.sharedVariables.Formulas.Add(nameSplit[0], nameSplit[1]);
@@ -45,39 +45,109 @@ public class Parsers
             switch (test)
             {
                 case '+': // +
-                numToAdd = Convert.ToDouble(numToAddArray);
-                Formulas.Add(lastOperation, numToAdd);
-                lastOperation = "+";
-                segmentNum++;
-                break;
+                    numToAdd = makeDouble(numToAddArray);
+                    Formulas.Add(lastOperation, numToAdd);
+                    lastOperation = "+";
+                    segmentNum++;
+                    break;
                 case '-': // -
 
-                break;
+                    break;
                 case '*': // *
 
-                break;
+                    break;
                 case '/': // /
 
-                break;
+                    break;
                 case '(': // (
 
-                break;
+                    break;
                 case ')': // )
 
-                break;
+                    break;
                 case '^':
-                // ^=
-                break;
+                    // ^=
+                    break;
                 case 's': // Sin
-                i = i + 3;
-                lastOperation = "Sin(";
-                break;
+                    i = i + 3;
+                    lastOperation = "Sin(";
+                    break;
                 default:
-                numToAddArray[numAdd] = test;
-                break;
+                    numToAddArray[numAdd] = test;
+                    break;
+            }
+        }
+    }
+    public static double makeDouble(char[] numberSegment)
+    {
+        int[] commaLocationArray = new int[0];
+
+        for (int i = 0; i < numberSegment.Length; i++)
+        {
+            char test = numberSegment[i];
+
+            if (test == '.')
+            {
+                commaLocationArray[0] = i;
             }
         }
 
+        char[] beforeComma = new char[numberSegment.Length];
+        char[] afterComma = new char[numberSegment.Length];
+
+        int beforeCommaLoc = 0;
+        int afterCommaLoc = 0;
+
+        for (int i = 0; i <= numberSegment.Length; i++)
+        {
+            int comLoc = commaLocation;
+
+            if (i < commaLocation)
+            {
+                beforeComma[beforeCommaLoc] = numberSegment[i];
+                beforeCommaLoc++;
+            }
+            else if (i > commaLocation)
+            {
+                afterComma[afterCommaLoc] = numberSegment[i];
+                afterCommaLoc++;
+            }
+        }
+
+        beforeCommaLoc = beforeComma.Length;
+        afterCommaLoc = afterComma.Length;
+
+        char[] beforeCommaShort = new char[beforeCommaLoc];
+        char[] afterCommaShort = new char[afterCommaLoc];
+
+        for (int i = 0; i < beforeCommaLoc; i++)
+        {
+            beforeCommaShort[i] = beforeComma[i];
+        }
+        for (int i = 0; i < afterCommaLoc; i++)
+        {
+            afterCommaShort[i] = afterComma[i];
+        }
+        
+        Console.WriteLine(beforeCommaLoc);
+
+        for (int i = 0; i < beforeCommaLoc; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(beforeCommaShort[i]);
+            Thread.Sleep(500);
+        }
+
+
+        // double numberFinal = int.Parse(string.Concat(beforeCommaShort)) + '.' + int.Parse(string.Concat(afterCommaShort));
+
+        // concat
+
+        double numberFinal = 2;
+
+        Console.WriteLine(numberFinal);
         Console.ReadKey();
+
+        return numberFinal;
     }
 }
