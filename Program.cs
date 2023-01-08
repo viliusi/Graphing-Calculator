4e0415fa29ef962ee0bc11b66044b3cbc38008c9
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-01-08 17:30:11 +0100

    Removed the "intro sequence"

--- Program.cs
+++ Program.cs
@@ -17,17 +17,6 @@
     }
     private static void Main(string[] args)
     {
-        Console.WriteLine(@"This is a Graphing Calculator made entirely with the use of C#
-Currently it is set to render 'Sin(x)'
-When the coordinate system appears, you will have the ability to interact with it
-WASD to move around and look at different parts of the system
-R to reset your view back to the orego
-Enter to just redraw the current setup
-        
-Once you are ready, press any button to continue");
-
-        Console.ReadKey();
-
         resetScreenPos();
         sharedVariables.scaleRatio = 4;
         updateOrego(ConsoleKey.Enter);
